d923a098ab6ad3c7c73b4bd10fd7c4e81c10724a
Benoit Perrier <bperrier@findmypast.com>
2020-12-14 16:40:38 +0000

    Fix tests

--- day12/navigation.spec.js
+++ day12/navigation.spec.js
@@ -4,14 +4,14 @@ describe('Ship', () => {
   describe('turnShip ', () => {
     it('should turn ship accordingly', () => {
       const ship = new Ship(0) // Start with a ship facing north
-      expect(ship.turnShip('R', 0).direction).toBe(0)
-      expect(ship.turnShip('R', 90).direction).toBe(90)
-      expect(ship.turnShip('R', 180).direction).toBe(270)
-      expect(ship.turnShip('R', 270).direction).toBe(180)
-      expect(ship.turnShip('L', 0).direction).toBe(180)
-      expect(ship.turnShip('L', 90).direction).toBe(90)
-      expect(ship.turnShip('L', 180).direction).toBe(270)
-      expect(ship.turnShip('L', 270).direction).toBe(0)
+      expect(ship.turnShip('R', 0).orientation).toBe(0)
+      expect(ship.turnShip('R', 90).orientation).toBe(90)
+      expect(ship.turnShip('R', 180).orientation).toBe(270)
+      expect(ship.turnShip('R', 270).orientation).toBe(180)
+      expect(ship.turnShip('L', 0).orientation).toBe(180)
+      expect(ship.turnShip('L', 90).orientation).toBe(90)
+      expect(ship.turnShip('L', 180).orientation).toBe(270)
+      expect(ship.turnShip('L', 270).orientation).toBe(0)
     })
   })
 
@@ -26,7 +26,7 @@ describe('Ship', () => {
   })
 
   describe('moveForward', () => {
-    it('sould move ship with directions accordingly', () => {
+    it('sould move ship with orientations accordingly', () => {
       const ship = new Ship(0)
       expect(ship.turnShip('R', 90).moveForward(10)).toMatchObject({ x: 10, y: 0})
       expect(ship.turnShip('R', 90).moveForward(10)).toMatchObject({ x: 10, y: -10})

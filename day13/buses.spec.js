d436dcbb9bc73be11dd3e94561710dc03ae87b05
Benoit Perrier <bperrier@findmypast.com>
2020-12-14 18:36:22 +0000

    Fix test

--- day13/buses.spec.js
+++ day13/buses.spec.js
@@ -4,7 +4,7 @@ describe('buses', () => {
   describe('getNextBus ', () => {
     it('should figure out what the next bus is', () => {
       const [timestamp, notes] = `939\n7,13,x,x,59,x,31,19`.split('\n')
-      expect(buses.getNextBus(timestamp, notes)[0]).toBe('59')
+      expect(buses.getNextBus(timestamp, notes)[0]).toEqual(["59", -5])
     })
   })
 })
